95884e39cbee07bd76889d37451f3ca825e8a5cf
An ByeongUk <quddnr005@naver.com>
2021-10-17 13:37:02 +0900

    hiring,trigger

--- custom.js
+++ custom.js
@@ -1,11 +1,26 @@
 $(function () {
   // Trigger
-  $(".trigger").click(function() {
+  $(".trigger").click(function () {
     $(this).toggleClass("active");
     $(".gnb").toggleClass("active");
   });
-  $("section").click(function() {
-    $('.gnb').removeClass('active')
-    $('.trigger').removeClass('active')
-  })
+  $("section, .menu a").click(function () {
+    $(".gnb").removeClass("active");
+    $(".trigger").removeClass("active");
+  });
+
+  //  Smooth Scrolling
+  $(".menu a, .gototop").click(function () {
+    $.scrollTo(this.hash || 0, 900);
+  });
+
+  // Change With Scroll Header
+  let head = $("header").innerHeight();
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > head) {
+      $("header, .gototop").addClass("active");
+    } else {
+      $("header, .gototop").removeClass("active");
+    }
+  });
 });
